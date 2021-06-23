f72bab7803097d8a815d7db5f98814c5a0047114
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-06-23 13:00:31 +0100

    :bug: TW config and attempting to fix undefined TwitchData calls

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@ module.exports = {
   purge: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          twitter: "#1da1f2",
+        },
+        red: {
+          twitch: "#e91916",
+          youtube: "#ff0000",
+        },
+        purple: {
+          twitch: "#9146ff",
+        },
+        gray: {
+          github: "#333333",
+        },
+      },
+      fontFamily: {
+        heading: ["Sora", "sans-serif"],
+        body: ["Source Sans Pro", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        layout: "2fr 4fr",
+        layoutWide: "1fr 5fr",
+      },
+    },
   },
   variants: {
     extend: {},
