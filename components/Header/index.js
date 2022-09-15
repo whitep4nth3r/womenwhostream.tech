4bcdfbd152f731243f3288d2873e3ad2df915d2a
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-09-15 16:06:30 +0100

    Add total streamer count to header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,13 +1,15 @@
 import Styles from "@styles/Header.module.css";
 
-export default function Header({ onlineCount }) {
+export default function Header({ onlineCount, total }) {
   return (
     <header className={Styles.header}>
       <h1 className={Styles.header__title}>Women Who Stream Tech</h1>
       <p className={Styles.header__subtitle}>
         Welcome to a directory of Twitch science, tech, software and game development streamers.
       </p>
-      <h2 className={Styles.header__liveCount}>{onlineCount} streamers live</h2>
+      <h2 className={Styles.header__liveCount}>
+        {onlineCount} / {total} streamers live
+      </h2>
       <p className={Styles.header__subtitleBottom}>
         This site is really broken. If you'd like to help fix it, join{" "}
         <a className={Styles.header__subtitleLink} target="_blank" href="https://discord.gg/theclaw">
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import Footer from "@components/Footer";
 import Header from "@components/Header";
 import { NextSeo } from "next-seo";
 
-export default function Index({ onlineStreamers, offlineStreamers }) {
+export default function Index({ onlineStreamers, offlineStreamers, totalStreamersCount }) {
   return (
     <>
       <NextSeo
         title="Home"
         description={`Women Who Stream Tech is a directory of Twitch science, tech, software and game development streamers.`}
       />
-      <Header onlineCount={onlineStreamers.length} />
+      <Header onlineCount={onlineStreamers.length} total={totalStreamersCount} />
       <main>
         <StreamersGrid streamers={[...onlineStreamers, ...offlineStreamers]} />
       </main>
@@ -24,10 +24,13 @@ export async function getServerSideProps() {
   const onlineStreamers = await Contentful.getOnlineStreamers();
   const offlineStreamers = await Contentful.getOfflineStreamers();
 
+  const totalStreamersCount = onlineStreamers.length + offlineStreamers.length;
+
   return {
     props: {
       onlineStreamers,
       offlineStreamers,
+      totalStreamersCount,
     },
   };
 }
